916fe9ffdcc52bf6feddd1977d7edf7aaf787a14
KT <korea3702@302Lab.co.kr>
2022-10-28 15:45:54 +0900

    로딩씬 커밋

---
+++ Assets/Scripts/Controllers/LoadingController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingController : MonoBehaviour
+{
+    private static string _nextScene;
+
+    public static void LoadScene(string sceneName)
+    {
+        _nextScene = sceneName;
+        SceneManager.LoadScene("LoadingScene");
+    }
+    
+    void Start()
+    {
+        StartCoroutine("LoadSceneProcess");
+    }
+
+    IEnumerator LoadSceneProcess()
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(_nextScene);
+        // 로딩상황을 90%에서 멈춘다
+        op.allowSceneActivation = false;
+        while (!op.isDone)
+        {
+            yield return null;
+            if (op.progress > 0.8f)
+            {
+                yield return new WaitForSeconds(3f);
+                yield return op.allowSceneActivation = true;
+            }
+        }
+    }
+}
